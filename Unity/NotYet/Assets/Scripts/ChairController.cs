867e06772281d210387a1e84fad53c293c48c75c
Marijn <marijn@marijnzwemmer.com>
2015-04-26 15:25:31 +0200

    2 player control basics working

---
+++ Unity/NotYet/Assets/Scripts/ChairController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ChairController : MonoBehaviour {
+
+    public float maxHeight = 3;
+    public float resizeSpeed = 0.05f;
+
+    public Transform Ladder;
+    public Transform LadderBottom;
+
+    float ladderHeight = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = CrossPlatformInputManager.GetAxis("Vertical");
+
+        ladderHeight += (h * resizeSpeed);
+
+        ladderHeight = Mathf.Clamp(ladderHeight, 1, 3);
+
+        Ladder.localScale = new Vector3(1, ladderHeight, 1);
+        Ladder.position = LadderBottom.transform.position + new Vector3(0, ladderHeight, 0);
+
+	}
+}
---
+++ Unity/NotYet/Assets/Scripts/EasyJoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyJoint : MonoBehaviour {
+
+    public Transform ConnectTo;
+
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+       // offset = this.transform.position - ConnectTo.position;
+
+        offset = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        this.transform.position = (ConnectTo.transform.position + offset);
+
+
+
+	}
+}
