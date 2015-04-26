4a693014324b593ea13279a3293996a5df905915
Marijn <marijn@marijnzwemmer.com>
2015-04-26 17:04:39 +0200

    buggy version with enemies

---
+++ Unity/NotYet/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public List<Transform> EnemyPrefabs;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemies());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1.0f);
+
+        }
+        
+    }
+
+    void SpawnEnemy()
+    {
+        float randomX = Random.Range(-15, 15);
+        float randomY = 15;
+
+        Transform instance = (Transform)Instantiate(EnemyPrefabs[Random.Range(0, EnemyPrefabs.Count - 1)], new Vector3(randomX, randomY), Quaternion.identity);
+
+
+    }
+}
---
+++ Unity/NotYet/Assets/Scripts/FreezeLocalPosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeLocalPosition : MonoBehaviour {
+
+    Vector3 originalLocalPosition;
+	// Use this for initialization
+	void Start () {
+	    originalLocalPosition = this.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localPosition = originalLocalPosition;
+	}
+}
